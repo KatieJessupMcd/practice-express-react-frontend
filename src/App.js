c613f7184bf597a6c4d587ea5658f0fba6166f4a
KatieJessupMcD <katie.jessupmcd@gmail.com>
2019-03-09 17:52:38 -0800

    boilerplate app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import JokesApi from "./JokesApi";
+import JokeList from "./JokeList";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topJokes: [],
+      bottomJokes: [],
+      randomJokes: []
+    }
+    this.getRandomJokes = this.getRandomJokes.bind(this);
+  }
+
+  async componentDidMount() {
+    await this.getRandomJokes();
+  }
+
+  async getRandomJokes() {
+    const jokeRequests = [
+      JokesApi.getTopJokes(),
+      JokesApi.getWorstJokes(),
+      JokesApi.getRandomJokes()
+    ];
+    const [ topJokes, bottomJokes, randomJokes ] = await Promise.all(jokeRequests);
+    this.setState({ topJokes, bottomJokes, randomJokes });
+  }
+
   render() {
+    const { topJokes, bottomJokes, randomJokes } = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Cheesy Jokez 🧀
         </header>
+        <div>
+          <h1>Top Jokes</h1>
+          <JokeList jokes={topJokes} />
+        </div>
+        <div>
+          <h1>Top Jokes</h1>
+          <JokeList jokes={bottomJokes} />
+        </div>
+        <div>
+          <h1>Random Jokes</h1>
+          <button onClick={this.getRandomJokes} >Get Random Jokes</button>
+          <JokeList jokes={randomJokes} />
+        </div>
       </div>
     );
   }

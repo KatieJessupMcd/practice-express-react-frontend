724658f395fbfcbaa4bd87875374c169bf964602
KatieJessupMcD <katie.jessupmcd@gmail.com>
2019-03-11 10:28:21 -0700

    further stripped down boilerplate and added comments

--- src/App.js
+++ src/App.js
@@ -1,52 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
-import JokesApi from "./JokesApi";
-import JokeList from "./JokeList";
+// IMPORT components and API call file
+// import JokesApi from "./JokesApi";
+// import JokeList from "./JokeList";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      topJokes: [],
-      bottomJokes: [],
-      randomJokes: []
+      // set state
     }
-    this.getRandomJokes = this.getRandomJokes.bind(this);
+    // bind functions 
+    // this.getRandomJokes = this.getRandomJokes.bind(this);
   }
 
+  // componentDidMount - make any calls you need
   async componentDidMount() {
     await this.getRandomJokes();
   }
 
-  async getRandomJokes() {
-    const jokeRequests = [
-      JokesApi.getTopJokes(),
-      JokesApi.getWorstJokes(),
-      JokesApi.getRandomJokes()
-    ];
-    const [ topJokes, bottomJokes, randomJokes ] = await Promise.all(jokeRequests);
-    this.setState({ topJokes, bottomJokes, randomJokes });
-  }
+  // async getRandomJokes() {
+  //   const jokeRequests = [
+  //     JokesApi.getTopJokes(),
+  //     JokesApi.getWorstJokes(),
+  //     JokesApi.getRandomJokes()
+  //   ];
+  //   const [ topJokes, bottomJokes, randomJokes ] = await Promise.all(jokeRequests);
+  //   this.setState({ topJokes, bottomJokes, randomJokes });
+  // }
 
   render() {
-    const { topJokes, bottomJokes, randomJokes } = this.state;
+    // DECONSTRUCT whatever you need from state
+    // const { topJokes, bottomJokes, randomJokes } = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          Cheesy Jokez 🧀
+          
         </header>
         <div>
-          <h1>Top Jokes</h1>
-          <JokeList jokes={topJokes} />
-        </div>
-        <div>
-          <h1>Top Jokes</h1>
-          <JokeList jokes={bottomJokes} />
-        </div>
-        <div>
-          <h1>Random Jokes</h1>
-          <button onClick={this.getRandomJokes} >Get Random Jokes</button>
-          <JokeList jokes={randomJokes} />
+          {/* children components here */}
         </div>
       </div>
     );
